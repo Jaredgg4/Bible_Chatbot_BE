9df4e857438e3e87647d4c30f125e76af8288292
Jaredgg4 <jg70456898@gmail.com>
2025-11-26 20:26:57 -0700

    Created a function that gets all the books in the bible

--- main.py
+++ main.py
@@ -11,12 +11,18 @@ BIBLE_ID = "de4e12af7f28f599-02"
 
 @app.route('/api/bibles')
 def get_bibles():
-    # Get book and chapter from query parameters, default to GEN.1
     book = request.args.get('book', 'GEN')
     chapter = request.args.get('chapter', '1')
     
     chapter_id = f"{book}.{chapter}"
     url = f"https://rest.api.bible/v1/bibles/{BIBLE_ID}/chapters/{chapter_id}"
+    
+    response = requests.get(url, headers={"api-key": API_KEY})
+    return {"response": response.json()}
+
+@app.route("/api/books")
+def get_books():
+    url = f"https://rest.api.bible/v1/bibles/{BIBLE_ID}/books"
     
     response = requests.get(url, headers={"api-key": API_KEY})
     return {"response": response.json()}
