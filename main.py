b87642afa613d4d5da17f24af08bc664678ffc89
Jaredgg4 <jg70456898@gmail.com>
2025-12-09 16:20:16 -0700

    Initialized the DB

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 import os
+import psycopg2
 
 app = Flask(__name__)
 CORS(app)
 
+db_password = os.getenv("db_password")
 API_KEY = os.getenv("API_KEY")
 BIBLE_ID = "de4e12af7f28f599-02"
+
+con = psycopg2.connect(host= "localhost", user= "postgres", password= db_password, port= "5432", database= "postgres")
+
+cursor = con.cursor()
+
+
+con.commit()
+
+cursor.close()
+con.close()
 
 @app.route('/api/bibles')
 def get_bibles():
