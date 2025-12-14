f375ea639b813acf8de37ed397936d3ec6c7ed27
Jaredgg4 <jg70456898@gmail.com>
2025-12-13 22:40:06 -0700

    Use Gunicorn

--- backend/app.py
+++ backend/app.py
@@ -196,4 +196,5 @@ def get_books():
     return {"response": response.json()}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 4000))
+    app.run(host='0.0.0.0', port=port, debug=False)
